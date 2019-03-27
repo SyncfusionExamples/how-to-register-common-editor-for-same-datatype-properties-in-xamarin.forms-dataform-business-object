3377b2ca56321f40f90dccb8f7ee60089e2d1398
AjitAk <ajithkumar.senthilkumar@syncfusion.com>
2019-03-27 09:44:23 +0530

    Added Two samples for inbuilt editor and custom editor

---
+++ CustomEditor/RegisterCommonEditor/RegisterCommonEditor/RegisterCommonEditor/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Syncfusion.XForms.DataForm;
+using Syncfusion.XForms.DataForm.Editors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RegisterCommonEditor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            dataForm.RegisterEditor("Slider", new CustomSliderEditor(dataForm));
+            dataForm.RegisterEditor(typeof(int), "Slider");
+        }
+    }
+    public class CustomSliderEditor : DataFormEditor<Slider>
+    {
+        public CustomSliderEditor(SfDataForm dataForm) : base(dataForm)
+        {
+
+        }
+
+        protected override Slider OnCreateEditorView(DataFormItem dataFormItem)
+        {
+            return new Slider
+            {
+                Minimum = 0,
+                Maximum = 23,
+                MaximumTrackColor = Color.Gray,
+                MinimumTrackColor = Color.HotPink,
+                ThumbColor = Color.HotPink
+            };
+        }
+
+        protected override void OnInitializeView(DataFormItem dataFormItem, Slider view)
+        {
+            base.OnInitializeView(dataFormItem, view);
+
+        }
+    }
+}
---
+++ InbuiltEditor/RegisterCommonEditor/RegisterCommonEditor/RegisterCommonEditor/ViewModel/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace RegisterCommonEditor
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private DayHours dayHours;
+        #region Properties
+
+        public DayHours Hours
+        {
+            get { return dayHours; }
+            set { dayHours = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public ViewModel()
+        {
+            dayHours = new DayHours();
+        }
+
+        #endregion
+
+        #region intreface member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion      
+    }
+
+}
